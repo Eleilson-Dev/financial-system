16fc1376d3fde638879fd5506821a97febec27a8
Mxt@DEV <maxta.gamer@gmail.com>
2026-02-27 20:48:31 -0300

    update

--- src/shared/middlewares/VerifyAdm.middleware.ts
+++ src/shared/middlewares/VerifyAdm.middleware.ts
@@ -4,14 +4,14 @@ import { AppError } from "../errors/AppError.js";
 
 export class VerifyAdmin {
   static async execute(req: Request, res: Response, next: NextFunction) {
-    const { userId, companyId, role } = res.locals.encodedToken;
+    const { userId } = res.locals.encodedToken;
     const user = await prisma.user.findUnique({ where: { id: userId } });
 
     if (!user) {
       throw new AppError(401, "No users logged in");
     }
 
-    if (role !== "OWNER" && role !== "ADMIN") {
+    if (user.role !== "OWNER" && user.role !== "ADMIN") {
       throw new AppError(403, "Forbidden: user must be OWNER or ADMIN");
     }
 
